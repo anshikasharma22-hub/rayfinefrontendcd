584ba67d161f5b8a2abd619a8e237851c85eb3b7
anshikasharma22-hub <anshikasharma220715@gmail.com>
2026-06-08 11:04:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import './App.css'
+import "./App.css";
 
 console.log("INDEX JS IS RUNNING");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-    );
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
